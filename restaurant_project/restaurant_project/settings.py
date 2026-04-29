5e663ee5070e7c3b9d96a2a890c10336e51d7ff7
Ye Htut Win <https://github.com/yehtutwin-py>
2026-04-29 09:01:50 +0700

    Allowed_host added

--- restaurant_project/restaurant_project/settings.py
+++ restaurant_project/restaurant_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-2d5c-g0mznim(&w+y4lwz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost 127.0.0.1', 'haruki-restaurant-production.up.railway.app').split(' ')
+ALLOWED_HOSTS = ['localhost','127.0.0.1', 'haruki-restaurant-production.up.railway.app','.railway.app']
 
 
 # Application definition
